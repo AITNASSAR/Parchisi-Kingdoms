0e04c6d93813e9ed0d19c447dac2e3c05e03a90d
AITNASSAR <aitnassaryounes09@gmail.com>
2025-04-25 17:07:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Parchisi Kingdoms",
+    version="1.0",
+    description="Board game built with Kivy",
+    author="AITNASSAR Younes",
+    packages=["."],
+    include_package_data=True,
+)
