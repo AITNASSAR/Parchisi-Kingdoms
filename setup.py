7f5dac0e2686aef8f7be2eb7e8c346ce7ee43ebc
AITNASSAR <aitnassaryounes09@gmail.com>
2025-04-26 17:39:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-from setuptools import setup
+- name: Setup Android SDK
+  run: |
+    # ایجاد دایرکتوری‌های موردنیاز
+    mkdir -p $HOME/android-sdk/cmdline-tools
 
-setup(
-    name="Parchisi Kingdoms",
-    version="1.0",
-    description="Board game built with Kivy",
-    author="AITNASSAR Younes",
-    packages=["."],
-    include_package_data=True,
-)
+    # دانلود Android Command Line Tools
+    wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O sdk-tools.zip
+
+    # اکسترکت فایل زیپ
+    unzip -q sdk-tools.zip -d $HOME/android-sdk/cmdline-tools
+
+    # جابجایی برای داشتن ساختار صحیح
+    mv $HOME/android-sdk/cmdline-tools/cmdline-tools $HOME/android-sdk/cmdline-tools/latest
+
+    # اضافه کردن مسیر به PATH برای این step
+    export PATH=$HOME/android-sdk/cmdline-tools/latest/bin:$PATH
+
+    # ثبت PATH در محیط کلی GitHub Actions برای استفاده در مراحل بعدی
+    echo "PATH=$HOME/android-sdk/cmdline-tools/latest/bin:$PATH" >> $GITHUB_ENV
+
+    # تایید خودکار همه‌ی لایسنس‌های SDK
+    yes | sdkmanager --licenses
+
+    # نصب ابزارهای لازم برای build
+    sdkmanager --sdk_root=$HOME/android-sdk \
+      "platform-tools" \
+      "platforms;android-31" \
+      "build-tools;31.0.0" \
+      "ndk;25.1.8937393"
+
